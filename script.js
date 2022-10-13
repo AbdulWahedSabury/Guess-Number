ec02a00241d70dc2ba1b47850ec017df1c14b46f
Ab Sabury <sabury2020@gmail.com>
2022-10-13 11:12:21 -0700

    project copleted

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 'use strict';
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+function displayMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
+
+document.querySelector('.check').addEventListener('click', function () {
+  let guess = Number(document.querySelector('.guess').value);
+  if (!guess < 1) {
+    displayMessage('😒 Type a number !');
+  }
+  if (guess === secretNumber) {
+    displayMessage('👏 You Win !');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '🤷‍♀️ To high !' : '🤷‍♀️ To low !');
+
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('😜 You lost the game!');
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  displayMessage('Start guessing...');
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = 0;
+});
